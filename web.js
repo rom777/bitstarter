44d69781330e125cd7726e29fb5d76970c0d4508
Roman <nomail@compute.iternal>
2013-07-08 11:28:32 +0000

    Update webjs

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   buf = fs.readFileSync('index.html');
-  buf.toString();
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
